6e4f9a1324dfcd0302f7fddbb28fb2131a33bf81
JuPiTeR126 <jupiter126@gmail.com>
2017-09-20 16:33:04 +0200

    modified:   updatetierionnodes.sh 	modified:   versions

--- updatetierionnodes.sh
+++ updatetierionnodes.sh
@@ -13,25 +13,28 @@
 
 
 #settings
-#script assumes that the node runs with the same username on each node
-user=tierionnode
-#if credits are not in node logs, should this script spend a credit on a hash to find out credit?
-spendmode="1"
+user=tierionnode 	#script assumes that the node runs with the same username on each node
+spendmode="1"    	#if credits are not in node logs, should this script spend a credit on a hash to find out credit?
+sshcopyid="1"		#if set to 1, copies the ssh keys to nodes during addnode, else doesn't
 
 if [[ "$spendmode" = "1" ]]; then
 	command -v chp >/dev/null 2>&1 || { echo >&2 "spendmode is set on 1, which requires chainpoint-cli (chp) to be installed, please follow instructions at https://github.com/chainpoint/chainpoint-cli or set spendmode=0"; exit 1; }
 fi
 
-#Usage: you can use this script by simply starting it in bash
-# you can also automate the update with cron by calling "updatetierionnodes.sh cron"
-
-#if listfile doesn't exist, we create it
-if [[ ! -f nodelist.txt ]]; then
+if [[ "$(command -v tput)" != "" ]]; then  #configure tput variables for colors if tput is available, else suggest installing it
+	def=$(tput sgr0);bol=$(tput bold);red=$(tput setaf 1;tput bold);gre=$(tput setaf 2;tput bold);yel=$(tput setaf 3;tput bold);blu=$(tput setaf 4;tput bold);mag=$(tput setaf 5;tput bold);cya=$(tput setaf 6;tput bold)
+	echo "roses are$red red$def, sky is$blu blue$def, test is$cya test$def, and leaf is$gre green$def"
+else
+	echo -e "\033[1;31mtput is not installed: install it for pretty colors ;)\033[m"
+fi
+exit
+if [[ ! -f nodelist.txt ]]; then  #if listfile doesn't exist, we create it
 	touch nodelist.txt
 fi
 
-function f_reset_nodeaddress {
+function f_reset_nodeaddress { #resets nodeaddress and nodeethadd
 nodeaddress=""
+nodeethadd=""
 }
 
 #if you don't have ssh keys setup, generate some
@@ -40,23 +43,11 @@ if [[ ! -f ~/.ssh/id_rsa ]]; then
 	ssh-keygen -t rsa -b 8192
 fi
 
-function f_list_nodes {
-#for nodeaddress in $(cat nodelist.txt); do
-#	credits=""
-#	credits="$(ssh $user@$nodeaddress "cd ~/chainpoint-node && docker-compose logs -t | grep -i 'Credits'|tail -n 1|cut -f6 -d:|sed 's/ //'")"
-#	if [[ "$credits" = "" ]]; then
-#		if [[ "$spendmode" = "1" ]]; then
-#			chp submit -s http://$nodeaddress $(echo thierionstatus|sha256sum|cut -f1 -d" ")
-#			credits="$(ssh $user@$nodeaddress "cd ~/chainpoint-node && docker-compose logs -t | grep -i 'Credits'|tail -n 1|cut -f6 -d:|sed 's/ //'")"
-#			echo "Node $nodeaddress has $credits credits"
-#		else
-#			credits="na"
-#		fi
-#	else
-#		echo "Node $nodeaddress has $credits credits"
-#	fi
-#done
+function get_node_eth_add {
+nodeethadd="$(ssh -n $user@$nodeaddress "cd ~/chainpoint-node && grep NODE_TNT .env|cut -d= -f2")"
+}
 
+function f_list_nodes {
 IFS=$'\n' read -d '' -r -a lines < nodelist.txt
 for nodeaddress in "${lines[@]}"
 do
@@ -64,7 +55,7 @@ do
         credits="$(ssh -n $user@$nodeaddress "cd ~/chainpoint-node && docker-compose logs -t | grep -i 'Credits'|tail -n 1|cut -f6 -d:|sed 's/ //'")"
         if [[ "$credits" = "" ]]; then
                 if [[ "$spendmode" = "1" ]]; then
-                        chp submit -s http://$nodeaddress $(echo thierionstatus|sha256sum|cut -f1 -d" ") && sleep 1
+			chp submit -s http://$nodeaddress $(echo -n tierionstatus | shasum -a 256 | awk '{print toupper($1)}') && sleep 1
                         credits="$(ssh $user@$nodeaddress "cd ~/chainpoint-node && docker-compose logs -t | grep -i 'Credits'|tail -n 1|cut -f6 -d:|sed 's/ //'")"
                         echo "Node $nodeaddress has $credits credits"
                 else
@@ -74,10 +65,6 @@ do
                 echo "Node $nodeaddress has $credits credits"
         fi
 done
-
-
-
-
 f_reset_nodeaddress
 }
 
@@ -90,7 +77,9 @@ if [[ "$nodeaddress" != "" ]]; then
 		echo "$nodeaddress allready in list, not adding"
 	else
 		echo "$nodeaddress" >> nodelist.txt && echo "added $nodeaddress to list, adding ssh key now.  You will need to type the users password"
-		ssh-copy-id $user@$nodeaddress
+		if [[ "$sshcopyid" = "1" ]]: then
+			ssh-copy-id $user@$nodeaddress
+		fi
 	fi
 fi
 f_reset_nodeaddress
@@ -135,19 +124,31 @@ if [[ "$nodeaddress" != "" ]]; then
 fi
 }
 
-function f_update_nodes {
-while read nodeaddress; do
+function f_update_node {
+if [[ "$nodeaddress" = "" ]]; then
+	cat nodelist.txt
+	echo "Please give the address of the node you want to update"
+	read nodeaddress
+fi
+if [[ "$nodeaddress" != "" ]]; then
 	echo "updating $nodeaddress"
 	ssh $user@$nodeaddress "cd ~/chainpoint-node && nstatus=\"$(git pull|head -n1|grep Already)\" && if [[ \"$nstatus\" != \"\" ]];then make down && make up; fi"
-done < nodelist.txt
-f_reset_nodeaddress
+fi
+}
+
+function f_update_nodes {
+IFS=$'\n' read -d '' -r -a lines < nodelist.txt
+for nodeaddress in "${lines[@]}"
+do
+	f_update_node
+done
 }
 
 function m_main_menu {
 while [ 1 ]
 do
 	PS3='Choose a number: '
-	select choix in "listnodes" "addnode" "delnode" "update" "startnode" "stopnode" "quit"
+	select choix in "listnodes" "addnode" "delnode" "updatenode" "updateall" "startnode" "stopnode" "quit"
 	do
 		break
 	done
@@ -155,7 +156,8 @@ do
 		listnodes) 	f_list_nodes;;
 		addnode)	f_add_node;;
 		delnode)	f_del_node;;
-		update)		f_update_nodes;;
+		updatenode)	f_update_node;;
+		updateall)	f_update_nodes;;
 		startnode)	f_start_node && f_reset_nodeaddress;;
 		stopnode)	f_stop_node && f_reset_nodeaddress;;
 		quit)		exit ;;
