539db976ce426099666923e91ff319234e364442
JuPiTeR126 <jupiter126@gmail.com>
2017-09-22 23:43:46 +0200

    modified:   updatetierionnodes.sh

--- updatetierionnodes.sh
+++ updatetierionnodes.sh
@@ -64,9 +64,8 @@ nodeethadd="$(ssh -n $user@$nodeaddress "cd ~/chainpoint-node && grep NODE_TNT .
 }
 
 function f_get_node_state {
-#whichpoint=$(cat /dev/urandom| tr -dc 'a-c'|head -c 1)
-whichpoint="a"
-state="$(curl -s https://$whichpoint.chainpoint.org/nodes/$nodeethadd|cut -d} -f1|grep -o true | wc -w|tr -d ' ')"
+whichpoint=$(cat /dev/urandom| tr -dc 'a-c'|head -c 1)
+state=$(ssh $user@$nodeaddress "echo $whichnode.chainpoint.org/nodes/$nodeethadd|cut -d} -f1|grep -o true | wc -w|tr -d ' ')")
 if [[ "$state" = "4" ]]; then
 	nodestate="$gre$state$def"
 else
@@ -122,10 +121,10 @@ for nodeaddress in "${lines[@]}"
 do
 	sem -j +0
 	local nodeethadd; nodeethadd=$(ssh -n $user@$nodeaddress "cd ~/chainpoint-node && grep NODE_TNT .env|cut -d= -f2")
-	#local whichpoint; whichpoint=$(cat /dev/urandom| tr -dc 'a-c'|head -c 1)
-	local whichpoint; whichpoint="a"
-	local state; state=$(ssh $user@$nodeaddress "echo $(curl -s https://a.chainpoint.org/nodes/$nodeethadd|cut -d} -f1|grep -o true | wc -w|tr -d ' ')")
-	#local state; state=$(ssh $user@$nodeaddress 'wget -q https://a.chainpoint.org/nodes/$nodeethadd -O index.html && if [[ "$(cat index.html|grep RateLimited)" != "" ]];then cat index.html|grep RateLimited; else cat index.html |cut -f1 -d}|grep -o true | wc -w|tr -d " ";fi')
+	local whichpoint; whichpoint=$(cat /dev/urandom| tr -dc 'a-c'|head -c 1)
+	#local whichpoint; whichpoint="a"
+	local state; state=$(ssh $user@$nodeaddress "echo $whichpoint.chainpoint.org/nodes/$nodeethadd|cut -d} -f1|grep -o true | wc -w|tr -d ' ')")
+	#local state; state=$(ssh $user@$nodeaddress 'wget -q https://$(cat /dev/urandom| tr -dc 'a-c'|head -c 1).chainpoint.org/nodes/$nodeethadd -O index.html && if [[ "$(cat index.html|grep RateLimited)" != "" ]];then cat index.html|grep RateLimited; else cat index.html |cut -f1 -d}|grep -o true | wc -w|tr -d " ";fi')
 	local nodestate
 	if [[ "$state" = "4" ]]; then
 		nodestate="$gre$state$def"
